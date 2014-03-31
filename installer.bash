15f2fba241dab3415f30515b3fa19e4a4750b66e
Ken Farmer <kenfar@gmail.com>
2014-03-31 12:41:47 -0600

    Added .tmux.conf

--- installer.bash
+++ installer.bash
@@ -9,7 +9,7 @@
 
 dir=~/dotfiles # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
-files=".bashrc .vimrc .zshrc" # list of files/folders to symlink in homedir
+files=".bashrc .vimrc .zshrc .tmux.conf" # list of files/folders to symlink in homedir
 
 ##########
 
