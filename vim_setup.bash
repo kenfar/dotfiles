fb0a44409f70e5241eba387527f9bcfd06b94434
Ken <ken.farmer@protectwise.com>
2016-03-04 14:56:41 -0700

    initial add

---
+++ vim_setup.bash
@@ -0,0 +1,12 @@
+$!/usr/bin/env bash
+
+# pathogen:
+mkdir ~/.vim/bundle
+cd ~/.vim/bundle
+git clone git://github.com/tpope/vim-sensible.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/tmhedberg/SimpylFold.git
+git clone git://github.com/majutsushi/tagbar
+git clone https://github.com/scrooloose/syntastic.git
+git clone git://github.com/Lokaltog/vim-powerline.git
+git clone git://github.com/ervandew/supertab.git
